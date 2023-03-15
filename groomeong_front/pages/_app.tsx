7f27fd1180318029dfdd4b912a5f6a7c65bf49a9
MadHeo <tnlfl288@gmail.com>
2023-03-16 00:06:55 +0900

    feat: 라이브러리 설치

--- groomeong_front/pages/_app.tsx
+++ groomeong_front/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Global } from "@emotion/react";
+import { GlobalStyle } from "../src/commons/GlobalStyles.ts";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global styles={GlobalStyle}></Global>
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ groomeong_front/src/commons/GlobalStyles.ts/index.ts
@@ -0,0 +1,8 @@
+import { css } from "@emotion/react";
+
+export const GlobalStyle = css`
+  * {
+    margin: 0px;
+    box-sizing: border-box;
+  }
+`;
