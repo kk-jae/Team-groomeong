ce34f2bbb6e8d66939c91b175875f66fd14fde0e
MadHeo <tnlfl288@gmail.com>
2023-03-17 11:48:27 +0900

    feat: add figma token style

--- groomeong_front/pages/_app.tsx
+++ groomeong_front/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Global } from "@emotion/react";
 import { GlobalStyle } from "../src/commons/GlobalStyles";
 import { RecoilRoot } from "recoil";
 import ApolloSetting from "../src/components/commons/apollo";
+import Text from "../src/components/atomic/atoms";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
           <Global styles={GlobalStyle}></Global>
           {/* <Layout> */}
           <Component {...pageProps} />
+          <Text></Text>
           {/* </Layout> */}
         </>
       </ApolloSetting>
