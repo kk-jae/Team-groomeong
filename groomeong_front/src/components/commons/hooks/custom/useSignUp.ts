91024fd275330c25ef861c1e3443c676b2ee21c8
Jhoon <jaehoon0822@gmail.com>
2023-03-21 11:54:56 +0900

    develop: Upstream Fatch 이후 GROO-94 Branch 변경을 위한 Commit

---
+++ groomeong_front/src/components/commons/hooks/custom/useSignUp.ts
@@ -0,0 +1,71 @@
+import { ChangeEvent, useState } from "react";
+import { useFormContext } from "react-hook-form";
+
+const useSignUp = () => {
+  const { getValues } = useFormContext();
+  const [validation, setValidation] = useState({
+    authNumber: "",
+    time: 180,
+    emailAuth: false,
+    valid: false,
+    error: "",
+  });
+
+  const email = getValues("email");
+
+  // mutation 으로 검증 되면.. 밑의 if 조건문 작동
+  const onClickEmailAuth = () => {
+    if (email.includes("@")) {
+      setValidation((prev) => ({
+        ...prev,
+        emailAuth: true,
+      }));
+    } else {
+      setValidation((prev) => ({
+        ...prev,
+        error: "중복된 이메일 입니다.",
+      }));
+    }
+  };
+
+  const getTimer = (t: number) => {
+    if (t >= 0) {
+      const min = Math.floor(t / 60);
+      const sec = String(Math.floor(t % 60)).padStart(2, "0");
+      return `${min}:${sec}`;
+    }
+  };
+
+  const onChangeAuthNumber = (e: ChangeEvent<HTMLInputElement>) => {
+    setValidation((prev) => ({
+      ...prev,
+      authNumber: e.target.value,
+    }));
+  };
+
+  const onClickAuthValidate = () => {
+    if (validation.authNumber === "123") {
+      setValidation((prev) => ({
+        ...prev,
+        valid: true,
+        error: "",
+      }));
+    } else {
+      setValidation((prev) => ({
+        ...prev,
+        valid: false,
+        error: "인증번호가 다릅니다.",
+      }));
+    }
+  };
+  return {
+    onClickEmailAuth,
+    onChangeAuthNumber,
+    getTimer,
+    onClickAuthValidate,
+    validation,
+    setValidation,
+  };
+};
+
+export default useSignUp;
