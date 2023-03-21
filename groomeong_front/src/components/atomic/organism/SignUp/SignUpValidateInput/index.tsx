53f466c29cb2d3a399cd066fffc2a95023de23a2
Jhoon <jaehoon0822@gmail.com>
2023-03-21 11:54:49 +0900

    develop: Upstream Fatch 이후 GROO-94 Branch 변경을 위한 Commit

--- groomeong_front/src/components/atomic/organism/SignUp/SignUpValidateInput/index.tsx
+++ groomeong_front/src/components/atomic/organism/SignUp/SignUpValidateInput/index.tsx
@@ -6,73 +6,19 @@ import {
   Error,
   ValidateDiv,
 } from "../index.styles";
-import { ChangeEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Buttons } from "../../../atoms/Buttons";
-import { useFormContext } from "react-hook-form";
-
+import useSignUp from "../../../../commons/hooks/custom/useSignUp";
 
 const SignUpValidateInput = () => {
-  // const [validation, setValidation] = useState<boolean>(false);
-  // const [authNumber, setAuthNumber] = useState<string>();
-  // const [time, setTime] = useState<number>(180);
-  // const [emailAuth, setEmailAuth] = useState<Boolean>(false);
-  // const [error, setError] = useState<boolean>(false)
-  const [validation, setValidation] = useState({
-    authNumber: "",
-    time: 180,
-    emailAuth: false,
-    valid: false,
-    error: "",
-  });
-
-  const { getValues } = useFormContext();
-
-  const onClickEmailAuth = () => {
-    const email = getValues("email");
-    // mutation 으로 검증 되면.. 밑의 if 조건문 작동
-    if (email.includes("@")) {
-      setValidation((prev) => ({
-        ...prev,
-        emailAuth: true,
-      }));
-    } else {
-      setValidation((prev) => ({
-        ...prev,
-        error: "중복된 이메일 입니다.",
-      }));
-    }
-  };
-
-  const getTimer = (t: number) => {
-    if (t >= 0) {
-      const min = Math.floor(t / 60);
-      const sec = String(Math.floor(t % 60)).padStart(2, "0");
-      return `${min}:${sec}`;
-    }
-  };
-
-  const onChangeAuthNumber = (e: ChangeEvent<HTMLInputElement>) => {
-    setValidation((prev) => ({
-      ...prev,
-      authNumber: e.target.value,
-    }));
-  };
-
-  const onClickAuthValidate = () => {
-    if (validation.authNumber === "123") {
-      setValidation((prev) => ({
-        ...prev,
-        valid: true,
-        error: "",
-      }));
-    } else {
-      setValidation((prev) => ({
-        ...prev,
-        valid: false,
-        error: "인증번호가 다릅니다.",
-      }));
-    }
-  };
+  const {
+    validation,
+    setValidation,
+    getTimer,
+    onChangeAuthNumber,
+    onClickAuthValidate,
+    onClickEmailAuth,
+  } = useSignUp();
 
   useEffect(() => {
     if (validation.emailAuth) {
@@ -94,7 +40,7 @@ const SignUpValidateInput = () => {
       }, 1000);
       return () => clearInterval(id);
     }
-  }, [validation]);
+  }, [setValidation, validation]);
 
   console.log(validation);
   return (
@@ -104,9 +50,11 @@ const SignUpValidateInput = () => {
           placeholder="인증번호를 입력해주세요."
           disabled={!validation.emailAuth as boolean}
           onChange={onChangeAuthNumber}
-          error={validation.error }
+          error={validation.error}
         />
-        <Label>{validation.emailAuth ? getTimer(validation.time) : "인증번호"}</Label>
+        <Label>
+          {validation.emailAuth ? getTimer(validation.time) : "인증번호"}
+        </Label>
         <div style={{ marginTop: "1rem" }}>
           <Error>{validation.error}</Error>
         </div>
