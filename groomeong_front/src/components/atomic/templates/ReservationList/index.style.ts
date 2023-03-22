d8f98c5b84e53a41fe89ac6b8819305de4da3b22
MadHeo <tnlfl288@gmail.com>
2023-03-22 18:19:52 +0900

    feat: 예약 리스트 페이지 제작

---
+++ groomeong_front/src/components/atomic/templates/ReservationList/index.style.ts
@@ -0,0 +1,27 @@
+import styled from "@emotion/styled";
+
+export const BodyWrapper = styled.div`
+  width: 1124px;
+  height: 1121px;
+  padding: 0px 64px;
+  background-color: white;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const BodyTopWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const BodyContentWrapper = styled.div`
+  width: 100%;
+  padding: 0px 64px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
