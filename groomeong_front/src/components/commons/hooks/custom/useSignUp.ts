341a6308766923c36db0982b89706d72fcafb391
JJHaEun <yj05037@gmail.com>
2023-03-22 18:45:43 +0900

    feat: 회원가입 기능 개발 (api 연동)

--- groomeong_front/src/components/commons/hooks/custom/useSignUp.ts
+++ groomeong_front/src/components/commons/hooks/custom/useSignUp.ts
@@ -1,77 +1,14 @@
 import { Modal } from "antd";
-import { ChangeEvent, useState } from "react";
-import { useFormContext } from "react-hook-form";
 import { IMutationCreateUserArgs } from "../../../../commons/types/generated/types";
 import { UseMutationCreateUser } from "../mutation/UseMutationCreateUser";
 
 export const useSignUp = () => {
   const [createUser] = UseMutationCreateUser();
-  const { getValues } = useFormContext();
-  const [validation, setValidation] = useState({
-    authNumber: "",
-    time: 180,
-    emailAuth: false,
-    valid: false,
-    error: "",
-  });
-
-  const email = getValues("email");
-
-  // mutation 으로 검증 되면.. 밑의 if 조건문 작동
-  const onClickEmailAuth = () => {
-    if (email.includes("@")) {
-      setValidation((prev) => ({
-        ...prev,
-        emailAuth: true,
-      }));
-    } else if (email === "") {
-      setValidation((prev) => ({
-        ...prev,
-        error: "잘못된 이메일형식 입니다.",
-      }));
-    } else {
-      setValidation((prev) => ({
-        ...prev,
-        error: "중복된 이메일 입니다.",
-      }));
-    }
-  };
-
-  const getTimer = (t: number) => {
-    if (t >= 0) {
-      const min = Math.floor(t / 60);
-      const sec = String(Math.floor(t % 60)).padStart(2, "0");
-      return `${min}:${sec}`;
-    }
-  };
-
-  const onChangeAuthNumber = (e: ChangeEvent<HTMLInputElement>) => {
-    setValidation((prev) => ({
-      ...prev,
-      authNumber: e.target.value,
-    }));
-  };
-
-  const onClickAuthValidate = () => {
-    if (validation.authNumber === "123") {
-      setValidation((prev) => ({
-        ...prev,
-        valid: true,
-        error: "",
-      }));
-    } else {
-      setValidation((prev) => ({
-        ...prev,
-        valid: false,
-        error: "인증번호가 다릅니다.",
-      }));
-    }
-  };
   const onClickSignUp = async (
     data: IMutationCreateUserArgs
   ): Promise<void> => {
     try {
-      const result = await createUser({
+      await createUser({
         variables: {
           name: data.name,
           email: data.email,
@@ -79,20 +16,12 @@ export const useSignUp = () => {
           phone: data.phone,
         },
       });
-      console.log(result);
       // 회원가입 성공 Modal
     } catch (error) {
       if (error instanceof Error) Modal.error({ content: error.message });
     }
   };
-
   return {
-    onClickEmailAuth,
-    onChangeAuthNumber,
-    getTimer,
-    onClickAuthValidate,
-    validation,
-    setValidation,
     onClickSignUp,
   };
 };
