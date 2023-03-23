f19523debfe31e86d3810de8fa375e806d9c13c2
권현재 <kk_jae@naver.com>
2023-03-23 10:01:11 +0900

    feat: login, sendEmail, DogSelctBox

--- groomeong_front/src/components/units/DogSelect/index.tsx
+++ groomeong_front/src/components/units/DogSelect/index.tsx
@@ -1,50 +1,28 @@
-import type { MouseEvent } from "react";
-import { useState } from "react";
-import * as S from "./index.styled";
+import { Select } from "antd";
 
 const dogs = [
-  { name: "멍재훈" },
-  { name: "멍광기" },
-  { name: "멍하은" },
-  { name: "멍현재" },
+  { value: "멍재훈" },
+  { value: "멍광기" },
+  { value: "멍하은" },
+  { value: "멍현재" },
 ];
 
-export const DogSelect = () => {
-  const [isOpenSelect, setIsOpenSelect] = useState(false);
-  const [choiceDog, setChoice] = useState("댕댕이를 선택해주세요");
-
-  const onClickOpenSelect = () => {
-    setIsOpenSelect((prev) => !prev);
-  };
-
-  const onClickChoiceDog = (event: MouseEvent<HTMLSpanElement>) => {
-    setChoice(event.currentTarget.id);
-    setIsOpenSelect((prev) => !prev);
+export const DogSelect = (props: any) => {
+  const onChange = (value: string) => {
+    // console.log(`${value}`);
+    props.setDog(value);
   };
 
   return (
-    <S.SelectContainer>
-      <S.SelectWrapper onClick={onClickOpenSelect}>
-        <S.SelectTitle>
-          <S.SelectTitleMessage choiceDog={choiceDog}>
-            {choiceDog}
-          </S.SelectTitleMessage>
-          <S.SelectTitleIcon>▼</S.SelectTitleIcon>
-        </S.SelectTitle>
-      </S.SelectWrapper>
-      {isOpenSelect ? (
-        <S.SelectItemWrapper>
-          {dogs.map((el, index) => (
-            <div key={index}>
-              <S.SelectItem id={el.name} onClick={onClickChoiceDog}>
-                {el.name}
-              </S.SelectItem>
-            </div>
-          ))}
-        </S.SelectItemWrapper>
-      ) : (
-        <div></div>
-      )}
-    </S.SelectContainer>
+    <Select
+      style={{ width: 428 }}
+      size="large"
+      showSearch
+      placeholder="댕댕이를 선택해주세요"
+      optionFilterProp="children"
+      onChange={onChange}
+      // options={[{ value: dogs[0].name }, { value: "아놔" }]}
+      options={[...dogs]}
+    />
   );
 };
