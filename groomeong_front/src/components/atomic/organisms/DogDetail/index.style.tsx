cd21f23bd767297bdab84783bd42977376921ddd
MadHeo <tnlfl288@gmail.com>
2023-03-26 20:36:40 +0900

    fix: 리뷰 한번 작성 후 버튼 diable되는 기능 개발

--- groomeong_front/src/components/atomic/organisms/DogDetail/index.style.tsx
+++ groomeong_front/src/components/atomic/organisms/DogDetail/index.style.tsx
@@ -30,16 +30,29 @@ export const DogDetailWrapper = styled.div`
 `;
 export const DogDetailContentWrapper = styled.div`
   display: flex;
+  flex-direction: row;
   justify-content: space-between;
-  align-items: center;
+  align-items: flex-start;
   width: 100%;
   padding: 32px 128px;
 `;
+
+export const DogDetailImgLabel = styled.div`
+  ${GS.Label.Medium};
+  color: ${GS.border.borderPrimary};
+  display: flex;
+  flex-direction: column;
+
+  div {
+    margin-bottom: 28px;
+  }
+`;
+
 export const DogDetailContentImg = styled.div<DogDetailContentImgProps>`
   width: 388px;
   height: 388px;
   padding: 32px;
-  background-image: url(${({ url }) => url ?? "/image/img-dog-detail.png"});
+  background-image: url(${({ url }) => url});
   background-position: center;
   background-repeat: no-repeat;
   background-size: contain;
