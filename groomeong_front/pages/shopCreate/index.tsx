34349ba5ff6008c6eb8ca2f77b25dafec1e00087
MadHeo <tnlfl288@gmail.com>
2023-03-27 10:14:03 +0900

    feat: 샵 등록 페이지 제작

---
+++ groomeong_front/pages/shopCreate/index.tsx
@@ -0,0 +1,5 @@
+import ShopCreateTemplate from "../../src/components/atomic/templates/ShopCreate";
+
+export default function ShopCreate() {
+  return <ShopCreateTemplate />;
+}
---
+++ groomeong_front/src/components/atomic/templates/ShopCreate/index.tsx
@@ -0,0 +1,60 @@
+import styled from "@emotion/styled";
+import { FormProvider, useForm } from "react-hook-form";
+import { UseMutationCreateShop } from "../../../commons/hooks/mutation/UseMutationCreateShop";
+const MainDiv = styled.div`
+  width: 1000px;
+  height: 1000px;
+  background-color: white;
+  padding: 100px;
+`;
+
+const Input = styled.input`
+  width: 300px;
+  height: 30px;
+  background-color: white;
+  margin-bottom: 25px;
+`;
+
+export default function ShopCreateTemplate() {
+  const [] = UseMutationCreateShop();
+
+  const method = useForm({
+    mode: "onChange",
+  });
+
+  const onclickRegister = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <FormProvider {...method}>
+        <form onSubmit={method.handleSubmit(onclickRegister)}>
+          <MainDiv>
+            가게 이름
+            <div>
+              <Input name={"name"} />
+            </div>
+            전화번호{" "}
+            <div>
+              <Input name={"phone"} />
+            </div>
+            오픈 시간
+            <div>
+              <Input name={"openTime"} />
+            </div>
+            클로즈 시간
+            <div>
+              <Input name={"closeTime"} />
+            </div>
+            주소
+            <div>
+              <Input name={"address"} />
+            </div>
+            <button onClick={onclickRegister}>가게 등록</button>
+          </MainDiv>
+        </form>
+      </FormProvider>
+    </>
+  );
+}
