9bd2240fe8ad43517935bab30d32601bbfc0d411
Jhoon <jaehoon0822@gmail.com>
2023-03-29 12:48:09 +0900

    fix: MapOveralyView Button 에 상세보기 링크 연결 완료 및 Recoil state 수정

--- groomeong_front/src/components/commons/hooks/custom/useMapOverlayView.ts
+++ groomeong_front/src/components/commons/hooks/custom/useMapOverlayView.ts
@@ -1,11 +1,14 @@
+import { useMoveToPage } from './useMovedToPage';
 import { mapState } from "./../../../../commons/Store/index";
 import { useRecoilValue } from "recoil";
 
 const useMapOverlayView = () => {
   const mapInfo = useRecoilValue(mapState);
+  const { onClickMoveToPage } = useMoveToPage()
 
   return {
     mapInfo,
+    onClickMoveToPage,
   };
 };
 
