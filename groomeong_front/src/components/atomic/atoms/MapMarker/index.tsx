2c7e914c96a0c9781b334d1b9f6b1fbc9b658f40
JJHaEun <yj05037@gmail.com>
2023-04-02 11:53:54 +0900

    fix: MapMarker, MapOverlayView 타입수정(? 추가)

--- groomeong_front/src/components/atomic/atoms/MapMarker/index.tsx
+++ groomeong_front/src/components/atomic/atoms/MapMarker/index.tsx
@@ -8,28 +8,32 @@ import { useRecoilValue } from "recoil";
 import { mapState } from "../../../../commons/Store";
 
 interface IMarkerProps extends MarkerProps {
-  shop: IAutocompleteShopsOutput;
+  shop?: IAutocompleteShopsOutput;
 }
 
 const MapMarker = (props: IMarkerProps) => {
-  const { onClickMaker,  markerRef } = useMaker(props.shop);
+  const { onClickMaker, markerRef } = useMaker(props.shop);
   const mapInfo = useRecoilValue(mapState);
-  const isClicked = isEqual(mapInfo?.shop?.id, props.shop.id);
+  const isClicked = isEqual(mapInfo?.shop?.id, props.shop?.id);
 
   return (
     <Marker
       ref={markerRef}
       position={props.position}
       zIndex={1000}
-      title={props.shop.name}
+      title={props.shop?.name}
       icon={{
         url: "/image/icon-marker.svg",
         scaledSize: new google.maps.Size(64, 64),
       }}
       onClick={onClickMaker}
-      animation={isClicked ? 1 : undefined}
+      animation={isClicked === true ? 1 : undefined}
     >
-      <MapOverlayView shop={props.shop} isClicked={isClicked} position={props.position} />
+      <MapOverlayView
+        shop={props.shop}
+        isClicked={isClicked}
+        position={props.position}
+      />
     </Marker>
   );
 };
--- groomeong_front/src/components/commons/hooks/custom/useMapOverlayView.ts
+++ groomeong_front/src/components/commons/hooks/custom/useMapOverlayView.ts
@@ -2,7 +2,7 @@ import { useMoveToPage } from "./useMovedToPage";
 import { UseQueryFetchShop } from "../query/UseQueryFetchShop";
 import { IAutocompleteShopsOutput } from "../../../../commons/types/generated/types";
 
-const useMapOverlayView = (shop: IAutocompleteShopsOutput) => {
+const useMapOverlayView = (shop?: IAutocompleteShopsOutput) => {
   console.log(shop);
   const { data } = UseQueryFetchShop(shop?.shopid);
   const { onClickMoveToPage } = useMoveToPage();
