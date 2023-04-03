80e32accf2ca69ec8462653b6c9d9a80f0542a1c
JJHaEun <yj05037@gmail.com>
2023-04-03 12:30:56 +0900

    fix

--- groomeong_front/next.config.js
+++ groomeong_front/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   trailingSlash: true,
+  historyApiFallback: true,
   generateBuildId: () => "project-groomeong",
 };
 
