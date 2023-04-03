ce330e0e99339a2d4fd4226e3f4d0946e1277ca0
JJHaEun <yj05037@gmail.com>
2023-04-03 12:41:57 +0900

    refix

--- groomeong_front/next.config.js
+++ groomeong_front/next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   trailingSlash: true,
-  historyApiFallback: true,
   generateBuildId: () => "project-groomeong",
 };
 
