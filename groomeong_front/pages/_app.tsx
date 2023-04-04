2fe7ecdab42b8dfb47d4e02b0e740a212df0df31
권현재 <kk_jae@naver.com>
2023-04-04 13:10:31 +0900

    fix: Merge Test

--- groomeong_front/pages/_app.tsx
+++ groomeong_front/pages/_app.tsx
@@ -4,7 +4,7 @@ import { Global } from "@emotion/react";
 import { GlobalStyle } from "../src/commons/GlobalStyles";
 import { RecoilRoot } from "recoil";
 import ApolloSetting from "../src/components/commons/apollo";
-import v8 from 'v8'
+import v8 from "v8";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
@@ -12,15 +12,11 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
       <ApolloSetting>
         <>
           <Global styles={GlobalStyle}></Global>
-          {/* <Layout> */}
           <Component {...pageProps} />
-          {/* </Layout> */}
         </>
       </ApolloSetting>
     </RecoilRoot>
   );
 }
 
-export async function getServerSideProps() {
-  console.log(v8.getHeapStatistics())
-}
+export async function getServerSideProps() {}
