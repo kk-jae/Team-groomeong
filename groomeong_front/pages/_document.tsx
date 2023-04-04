e6f99b2678b2763079f1b4068c5e0175174a0b11
권현재 <kk_jae@naver.com>
2023-04-04 12:54:03 +0900

    feat: Head 수정

--- groomeong_front/pages/_document.tsx
+++ groomeong_front/pages/_document.tsx
@@ -4,6 +4,17 @@ export default function Document(): JSX.Element {
   return (
     <Html lang="en">
       <Head />
+      <meta property="og:title" content="GROOMEONG" />
+      <meta
+        property="og:description"
+        content="반려견을 더욱 사랑스럽게, 더욱 건강하게, GROOMEONG"
+      />
+      <link
+        rel="shortcut icon"
+        type="image/x-icon"
+        href="/image/icon-dog.svg"
+      />
+      <title>반려견을 더욱 사랑스럽게, 더욱 건강하게, GROOMEONG</title>
       <body>
         <Main />
         <NextScript />
